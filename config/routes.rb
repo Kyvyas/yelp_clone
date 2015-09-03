786cae6f1c0c6728ff43abf2981e8c71fe86bdaf
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-09-03 10:48:12 +0100

    Added fb authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
 
-  get 'restaurants' => 'restaurants#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
+  end
+
+  root to: "restaurants#index"
+
   resources :restaurants do
     resources :reviews
+  end
+
+  resources :users do
+    resources :restaurants
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
